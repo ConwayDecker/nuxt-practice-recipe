f4dc80d438344eda449dd4e52efd7ed8f77e86d9
Jason Sagalow <jrssagalow@gmail.com>
2025-02-20 16:55:52 +0900

    feat: nuxt modules setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+
   future: {
     compatibilityVersion: 4,
   },
+
   experimental: {
       scanPageMeta: 'after-resolve',
       sharedPrerenderData: false,
@@ -19,13 +21,28 @@ export default defineNuxtConfig({
         }
       }
     },
-    features: {
-      inlineStyles: true
+
+  features: {
+    inlineStyles: true
+  },
+
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false
+    }
+  },
+
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts',
+    '@nuxt/icon',
+    '@nuxt/image'
+  ],
+
+  googleFonts :{
+    families: {
+      Montserrat: true,
     },
-    unhead: {
-      renderSSRHeadOptions: {
-        omitLineBreaks: false
-      }
-    },
-  devtools: { enabled: true }
+  },
 })
